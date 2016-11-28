7b0a9995f0ea967280a2e0f7395cc6f73da64855
dorukkorkmaz <dorukkorkmaz@172.20.80.243>
2016-11-28 22:57:09 +0200

    Ball & Engel & Player

--- src/game/Player.java
+++ src/game/Player.java
@@ -4,4 +4,26 @@ package game;
  * Created by ASEN14 on 28.11.2016.
  */
 public class Player {
+	String name;
+	int score;
+	
+	public Player(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
 }
