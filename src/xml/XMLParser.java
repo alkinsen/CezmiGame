743b011e88715c5fb03618ae07afe46eff4ed5ff
alikucukdeveci <alikucukdeveci@172.20.116.106>
2016-12-09 20:08:24 +0200

    XML Parsing implemented.

--- src/xml/XMLParser.java
+++ src/xml/XMLParser.java
@@ -3,6 +3,7 @@ package xml;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -12,254 +13,258 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
 
-import game.Ball;
-import game.Takoz;
-import game.SquareTakoz;
-import game.TriangleTakoz;
-import game.Tokat;
-import game.Firildak;
-import game.Cezerye;
-
-
-import game.LeftTokat;
-import game.RightTokat;
-import game.Cezmi;
-
 public class XMLParser {
-	Ball CezmiBall = new Ball();
-	
-	ArrayList<SquareTakoz> CezmiSquareTakoz = new ArrayList<SquareTakoz>();
-	ArrayList<TriangleTakoz> CezmiTriangleTakoz = new ArrayList<TriangleTakoz>();
-	ArrayList<Tokat> CezmiTokat = new ArrayList<Tokat>();
-	ArrayList<Firildak> CezmiFirildak = new ArrayList<Firildak>();
-	ArrayList<Cezerye> CezmiCezerye = new ArrayList<Cezerye>();
-	ArrayList<LeftTokat> CezmiLeftTokat = new ArrayList<LeftTokat>();
-	ArrayList<RightTokat> CezmiRightTokat = new ArrayList<RightTokat>();
-	
-	Cezmi HadiCezmi1 = new Cezmi();
-	Cezmi HadiCezmi2 = new Cezmi();
+
+	private Document doc;
 	File CezmiFile;
-	Boolean validity = true; 
-	
-   public XMLParser(File CezmiCezmiFile){
-	   this.CezmiFile = CezmiCezmiFile;
-	   CezmiBall = new Ball();
-	   CezmiSquareTakoz = new ArrayList<SquareTakoz>();
-	   CezmiTriangleTakoz = new ArrayList<TriangleTakoz>();
-	   CezmiTokat = new ArrayList<Tokat>();
-	   CezmiFirildak = new ArrayList<Firildak>();
-	   CezmiCezerye = new ArrayList<Cezerye>();
-	   CezmiLeftTokat = new ArrayList<LeftTokat>();
-	   CezmiRightTokat = new ArrayList<RightTokat>();
-	   
-	   validity = true;
-	   
-      try {	
-         File inputFile = CezmiCezmiFile;
-         DocumentBuilderFactory dbFactory 
-            = DocumentBuilderFactory.newInstance();
-         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(inputFile);
-         doc.getDocumentElement().normalize();
-         
-         NodeList nList = doc.getElementsByTagName("ball");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList.item(temp);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;
-               
-               CezmiBall.setVx(Double.parseDouble(eElement.getAttribute("xVelocity")));
-               CezmiBall.setVy(Double.parseDouble(eElement.getAttribute("yVelocity")));
-               
-               if(eElement.getAttribute("x")!=""){
-            	   CezmiBall.setX(Double.parseDouble(eElement.getAttribute("x")));
-            	   }
-               if(eElement.getAttribute("y")!=""){
-            	   CezmiBall.setY(Double.parseDouble(eElement.getAttribute("y")));
-            	   }
-            }
-         }
-         
-         NodeList nList1 = doc.getElementsByTagName("cezmi1");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList1.item(temp);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;   
-               
-               HadiCezmi1.setScore(Integer.parseInt(eElement.getAttribute("score")));   
-            
-               if(eElement.getAttribute("x")!=""){   
-            	   HadiCezmi1.setX(Double.parseDouble(eElement.getAttribute("x")));
-            }  
-               
-            }
-         }
-         
-         NodeList nList2 = doc.getElementsByTagName("cezmi2");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList2.item(temp);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;   
-               
-               HadiCezmi2.setScore(Integer.parseInt(eElement.getAttribute("score")));   
-            
-               if(eElement.getAttribute("x")!=""){   
-            	   HadiCezmi2.setX(Double.parseDouble(eElement.getAttribute("x")));
-            }  
-               
-            }
-         }
-         
-         NodeList nList3 = doc.getElementsByTagName("key1");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList2.item(temp);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;   
-              
-               key1.setKey(Integer.parseInt(eElement.getAttribute("key")));  
-               
-            }  
-               
-            }
-         
-         
-         NodeList nList4 = doc.getElementsByTagName("key2");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList2.item(temp);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;   
-              
-               key2.setKey(Integer.parseInt(eElement.getAttribute("key")));  
-               
-            }  
-               
-            }
-         
-         NodeList nList5 = doc.getElementsByTagName("squareTakoz");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList5.item(temp);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;   
-              
-               SquareTakoz.setKey(Integer.parseInt(eElement.getAttribute("key")));  
-               
-            }  
-               
-            }
 
 
-         
-         NodeList nList5 = doc.getElementsByTagName("takoz");
+	public XMLParser(File CezmiFile){
 
-         for (int temp = 0; temp < nList2.getLength(); temp++) {
-            Node nNode = nList5.item(temp);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;
-               Takoz takoz = new Takoz(Integer.parseInt(eElement.getAttribute("x")),Integer.parseInt(eElement.getAttribute("y")));
-               if(checkPosition(takoz)){
-            	   CezmiTakoz.add(takoz);
-               } else{
-            	   validity = false;
-               }
-               
-            }
-         }
-         
-         
-         
-         NodeList nList4 = doc.getElementsByTagName("tokat");
+		try {	
+			this.CezmiFile = CezmiFile;
+			DocumentBuilderFactory dbFactory 
+			= DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(CezmiFile);
+			doc.getDocumentElement().normalize();
 
-         for (int temp = 0; temp < nList4.getLength(); temp++) {
-            Node nNode = nList4.item(temp);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               Element eElement = (Element) nNode;
-               Tokat tokat = new Tokat(Integer.parseInt(eElement.getAttribute("x")),Integer.parseInt(eElement.getAttribute("y")));
-               if(checkPosition(tokat)){
-            	   CezmiTokat.add(tokat);
-               } else{
-            	   validity = false;
-               }
-               
-            }
-         }
-         
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+		}catch(Exception e){
+			System.out.println(e.toString());
+		} 
 
-   
-   //Checking invalid gizmo positions
-//private boolean checkPosition(Takoz takoz) {
-	// TODO Auto-generated method stub
-	//if(takoz.x < 0 || takoz.y <0 || takoz.x > 20 || takoz.y > 20 ) return false;
-	//for(int i=0; i<CezmiTakoz.size(); i++){
-		//Takoz temp = CezmiTakoz.get(i);
-		//if(temp.x == takoz.x && temp.y == takoz.y){
-			//return false;
-		//}
-	//}
-	//return true;
-//}
+	}
+
+	public ArrayList<HashMap<String, String>> createBallFromXml(){ 
+		ArrayList<HashMap<String, String>> ballList = new ArrayList<>();
+		HashMap<String, String> ballInfo = new HashMap<String, String>();
+		
+		try {
+			System.out.println("deneme");
+			NodeList nList = doc.getElementsByTagName("ball");
+			System.out.println(nList.getLength());
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				System.out.println("deneme2");
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					ballInfo.put("type", "ball");
+					ballInfo.put("x", eElement.getAttribute("x"));
+					ballInfo.put("y", eElement.getAttribute("y"));
+					ballInfo.put("vx", eElement.getAttribute("xVelocity"));
+					ballInfo.put("vy", eElement.getAttribute("yVelocity"));
+
+					System.out.println("deneme3");
+					ballList.add(ballInfo);
+				}
+
+			}
+
+		}catch(Exception e){
+			System.out.println(e.toString());
+		} 
+		return ballList;
+	}
+
+	public ArrayList<HashMap<String, String>> crateCezmi1FromXml() {
+		ArrayList<HashMap<String, String>> cezmiList = new ArrayList<>();
+		HashMap<String, String> cezmiInfo = new HashMap<String, String>();
+
+		try {
+			NodeList nList = doc.getElementsByTagName("cezmi1");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					cezmiInfo.put("type", "Cezmi");
+					cezmiInfo.put("x", eElement.getAttribute("x"));
+					cezmiInfo.put("y", eElement.getAttribute("y"));
+					cezmiInfo.put("score", eElement.getAttribute("score"));
+					cezmiInfo.put("name", eElement.getAttribute("name"));
+					cezmiInfo.put("leftKey", eElement.getAttribute("leftKey"));
+					cezmiInfo.put("rightKey", eElement.getAttribute("rightKey"));
+
+					cezmiList.add(cezmiInfo);
+				}  
+
+			}
+
+		}catch(Exception e){
+			System.out.println(e.toString());
+		} 
+		return cezmiList;
+	}
+
+	public ArrayList<HashMap<String, String>> crateCezmi2FromXml() {
+		ArrayList<HashMap<String, String>> cezmiList = new ArrayList<>();
+		HashMap<String, String> cezmiInfo = new HashMap<String, String>();
+
+		try {
+			NodeList nList = doc.getElementsByTagName("cezmi2");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					cezmiInfo.put("type", "Cezmi");
+					cezmiInfo.put("x", eElement.getAttribute("x"));
+					cezmiInfo.put("y", eElement.getAttribute("y"));
+					cezmiInfo.put("score", eElement.getAttribute("score"));
+					cezmiInfo.put("name", eElement.getAttribute("name"));
+					cezmiInfo.put("leftKey", eElement.getAttribute("leftKey"));
+					cezmiInfo.put("rightKey", eElement.getAttribute("rightKey"));
+
+					cezmiList.add(cezmiInfo);
+				}  
+
+			}
+
+		}catch(Exception e){
+			System.out.println(e.toString());
+		} 
+		return cezmiList;
+	}
+
+	public ArrayList<HashMap<String, String>> crateLevelFromXml() {
+		ArrayList<HashMap<String, String>> levelList = new ArrayList<>();
+		HashMap<String, String> levelInfo = new HashMap<String, String>();
+
+		try {
+			NodeList nList = doc.getElementsByTagName("level");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					levelInfo.put("type", "level");
+					levelInfo.put("level", eElement.getAttribute("level"));
 
 
-//private boolean checkPosition(Tokat tokat) {
-	// TODO Auto-generated method stub
-	//if(tokat.x < 0 || tokat.y <0 || tokat.x > 20 || tokat.y > 20 ) return false;
-	//for(int i=0; i<CezmiTokat.size(); i++){
-		//Takoz temp = CezmiTokat.get(i);
-		//if(temp.x == tokat.x && temp.y == tokat.y){
-			//return false;
-		//}
-	//}
-	//return true;
-//}
+					levelList.add(levelInfo);
+				}  
+
+			}
+
+		}catch(Exception e){
+			System.out.println(e.toString());
+		} 
+		return levelList;
+	}  
+
+	public ArrayList<HashMap<String, String>> crateGizmoFromXml() {
+		ArrayList<HashMap<String, String>> gizmoList = new ArrayList<>();
+		HashMap<String, String> gizmoInfo = new HashMap<String, String>();
+
+		try {
+			NodeList nList = doc.getElementsByTagName("squareTakoz");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					gizmoInfo.put("type", "gizmo");
+					gizmoInfo.put("x", eElement.getAttribute("x"));
+					gizmoInfo.put("y", eElement.getAttribute("y"));
+
+					gizmoList.add(gizmoInfo);
+					}
+			}
+
+				NodeList nList1 = doc.getElementsByTagName("triangleTakoz");
+				for (int temp1 = 0; temp1 < nList1.getLength(); temp1++) {
+					Node nNode1 = nList1.item(temp1);
+
+					if (nNode1.getNodeType() == Node.ELEMENT_NODE) {
+
+						Element eElement1 = (Element) nNode1;
+						gizmoInfo.put("type", "gizmo");
+						gizmoInfo.put("x", eElement1.getAttribute("x"));
+						gizmoInfo.put("y", eElement1.getAttribute("y"));
+						gizmoInfo.put("orientation", eElement1.getAttribute("orientation"));
+
+						gizmoList.add(gizmoInfo);
+						}
+				}
+					NodeList nList2 = doc.getElementsByTagName("triangleTakoz");
+					for (int temp2= 0; temp2 < nList2.getLength(); temp2++) {
+						Node nNode2 = nList2.item(temp2);
+
+						if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+
+							Element eElement2 = (Element) nNode2;
+							gizmoInfo.put("type", "gizmo");
+							gizmoInfo.put("x", eElement2.getAttribute("x"));
+							gizmoInfo.put("y", eElement2.getAttribute("y"));
+
+							gizmoList.add(gizmoInfo);
+							}	
+					}
+
+						NodeList nList3 = doc.getElementsByTagName("firildak");
+						for (int temp3= 0; temp3 < nList3.getLength(); temp3++) {
+							Node nNode3 = nList3.item(temp3);
+
+							if (nNode3.getNodeType() == Node.ELEMENT_NODE) {
+
+								Element eElement3 = (Element) nNode3;
+								gizmoInfo.put("type", "gizmo");
+								gizmoInfo.put("x", eElement3.getAttribute("x"));
+								gizmoInfo.put("y", eElement3.getAttribute("y"));
+								gizmoInfo.put("angle", eElement3.getAttribute("angle"));
+
+								gizmoList.add(gizmoInfo);
+								}
+						}
+
+							NodeList nList4 = doc.getElementsByTagName("leftTokat");
+							for (int temp4= 0; temp4 < nList4.getLength(); temp4++) {
+								Node nNode4 = nList4.item(temp4);
+
+								if (nNode4.getNodeType() == Node.ELEMENT_NODE) {
+
+									Element eElement4 = (Element) nNode4;
+									gizmoInfo.put("type", "gizmo");
+									gizmoInfo.put("x", eElement4.getAttribute("x"));
+									gizmoInfo.put("y", eElement4.getAttribute("y"));
+									gizmoInfo.put("orientation", eElement4.getAttribute("orientation"));
+
+									gizmoList.add(gizmoInfo);
+									}
+							}
+
+								NodeList nList5 = doc.getElementsByTagName("rightTokat");
+								for (int temp5= 0; temp5 < nList5.getLength(); temp5++) {
+									Node nNode5 = nList5.item(temp5);
+
+									if (nNode5.getNodeType() == Node.ELEMENT_NODE) {
+
+										Element eElement5 = (Element) nNode5;
+										gizmoInfo.put("type", "gizmo");
+										gizmoInfo.put("x", eElement5.getAttribute("x"));
+										gizmoInfo.put("y", eElement5.getAttribute("y"));
+										gizmoInfo.put("orientation", eElement5.getAttribute("orientation"));
+
+										gizmoList.add(gizmoInfo);
+										}
+								} 
+					
+
+							
+
+						}catch(Exception e){
+							System.out.println(e.toString());
+						} 
+						return gizmoList;
+					}
 
 
-public Ball getBall() {
-	return CezmiBall;
-}
-
-public void setBall(Ball CezmiBall) {
-	this.CezmiBall = CezmiBall;
-}
-
-
-public ArrayList<Takoz> getCezmiTakoz() {
-	return CezmiTakoz;
-}
-
-public void setCezmiTakoz(ArrayList<Takoz> CezmiTakoz) {
-	this.CezmiTakoz = CezmiTakoz;
-}
-
-public File getXMLFile() {
-	return CezmiFile;
-}
-
-public void setFile(File CezmiFile) {
-	this.CezmiFile = CezmiFile;
-}
-
-public Boolean getValidity() {
-	return validity;
-}
-
-public void setValidity(Boolean validity) {
-	this.validity = validity;
-}
-}
+				}
 
 
