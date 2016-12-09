e332ab708ee3071d2ddcbae6d36798fd8fdc28ac
dorukkorkmaz <dorukkorkmaz@172.16.100.37>
2016-12-09 17:11:38 +0200

    HadiCezmi is initialized Fields are added to Player

--- src/game/HadiCezmi.java
+++ src/game/HadiCezmi.java
@@ -1,5 +1,10 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import xml.XMLParser;
+
 /**
  * Created by ASEN14 on 28.11.2016.
  */
@@ -9,7 +14,181 @@ public class HadiCezmi {
     //GLOBAL VARIABLES SHOULD BE HERE
 	public static final int BOARD_WIDTH = 0;
 	public static final int BOARD_HEIGHT = 0;
+	
+	private Player player1;
+	private Player player2;
+	private Board board;
+	
+	public HadiCezmi(int level, String playerName1, String playerName2) {
+		super();
+		player1 = new Player(playerName1);
+		player2 = new Player(playerName2);
+		board = new Board(level);
+		
+	}
 
+	public void readXML(File file){
+		XMLParser xmlParser = new XMLParser(file);
+		
+		//creating ball from xml
+		ArrayList<HashMap<String, String>> ballList = xmlParser.createBallFromXml();
+		if (ballList.get(0).containsKey("x")  &&  ballList.get(0).containsKey("y")){
+			board.changeBallPosition(Integer.parseInt(ballList.get(0).get("x")), Integer.parseInt(ballList.get(0).get("y")));
+		}
+		else if (ballList.get(0).containsKey("vx")  &&  ballList.get(0).containsKey("vy")){
+			board.changeBallVelocity(Integer.parseInt(ballList.get(0).get("vx")), Integer.parseInt(ballList.get(0).get("vy")));
+		}
+		
+		//creating cezmi from xml
+		ArrayList<HashMap<String, String>> cezmiList = xmlParser.createCezmiFromXml();
+		if (cezmiList.get(0).containsKey("cezmiNumber") && cezmiList.get(0).containsKey("x") && cezmiList.get(0).containsKey("y")){
+			board.changeCezmiPosition(Integer.parseInt(cezmiList.get(0).get("cezmiNumber")), Integer.parseInt(cezmiList.get(0).get("x")), Integer.parseInt(cezmiList.get(0).get("y")));
+		}
+		
+		//creating gizmo from xml
+		ArrayList<HashMap<String, String>> gizmoList = xmlParser.createGizmoFromXml();
+		for (int i=0; i< gizmoList.size(); i++){
+			if (gizmoList.get(i).containsKey("type") && gizmoList.get(i).containsKey("x") && gizmoList.get(i).containsKey("y")){
+				board.changeCezmiPosition(Integer.parseInt(gizmoList.get(i).get("cezmiNumber")), Integer.parseInt(gizmoList.get(0).get("x")), Integer.parseInt(gizmoList.get(0).get("y")));
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	private boolean runningMode=false;
+	private boolean editMode=false;
+	private Timer t;
+	private eventListener el=new eventListener();
+	
+	public HadiCezmi(int level, String playerName1, String playerName2) {
+		super();
+		player1 = new Player(playerName1);
+		player2 = new Player(playerName2);
+		board = new Board(level);
+		t=new Timer(20,el);
+	}
+    
+
+    public void doAction(String s, String[] arg){
+    	int x=Integer.parseInt(arg[0]);
+    	int y=Integer.parseInt(arg[1]);
+    	if(editMode==true && runningMode==false){
+    	switch(s){
+    	
+    	case "addLeftTokat":
+    		board.addGizmo("leftTokat",x,y);
+    		break;
+    	case "addRightTokat":
+    		board.addGizmo("rightTokat",x,y);
+    		break;
+    	case "addSquareTakoz":
+    		board.addGizmo("squareTakoz", x,y);
+    		break;
+    	case "addTriangularTakoz":
+    		board.addGizmo("triangularTakoz", x,y);
+    		break;
+    	case "addFirildak":
+    		board.addGizmo("firildak", x, y);
+    		break;
+    	case "deleteSquareTakoz":
+    		board.deleteGizmo(x, y);
+    		break;
+    	case "deleteTriangularTakoz":
+    		board.deleteGizmo(x, y);
+    		break;
+    	case "deleteLeftTokat":
+    		board.deleteGizmo(x, y);
+    		break;
+    	case "deleteRightTokat":
+    		board.deleteGizmo(x, y);
+    		break;
+    	case "deleteFirildak":
+    		board.deleteGizmo(x, y);
+    		break;
+    	default:
+    		break;
+    	}
+    	}
+    }
+    
+    public void doAction(String s){
+    	switch(s){
+    	case "play":
+    		play();
+    		break;
+    	case "pause":
+    		pause();
+    		break;
+    	case "editMode":
+    		editMode();
+    		break;
+    		default:
+    			break;
+    	}
+    	
+    }
+    public void play(){
+    	runningMode=true;
+    	t.start();
+    	
+    }
+    
+    public void pause(){
+    	runningMode=false;
+    	t.stop();
+    }
+    
+    public void editMode(){
+    	editMode=true;
+    	runningMode=false;
+    	t.stop();
+    }
+    
+    class eventListener implements KeyListener,ActionListener{
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getKeyCode()){
+				board.moveCezmi(cezmiNumber, direction);
+			} 
+			else if(e.getKeyCode()){
+				board.moveCezmi(cezmiNumber, direction);
+				
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
+    }
+	
 
 
 
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,12 +1,21 @@
 package game;
 
+import java.util.Observable;
+
 /**
  * Created by ASEN14 on 28.11.2016.
  */
-public class Player {
+public class Player extends Observable{
 	String name;
 	int score;
 	int numOfEdit;
+	int numOfGizmo;
+
+
+	public Player(){
+		this.score = 0;
+		this.numOfEdit = 3;
+	}
 	
 	public Player(String name){
 		this.name = name;
@@ -32,6 +41,8 @@ public class Player {
 	
 	public void incrementScore(){
 		this.score += 1;
+		setChanged();
+		notifyObservers();
 	}
 	
 	public int getNumOfEdit() {
@@ -48,6 +59,27 @@ public class Player {
 	
 	public boolean checkNumOfEdit(){
 		if(numOfEdit >=1){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public int getNumOfGizmo() {
+		return numOfGizmo;
+	}
+
+	public void setNumOfGizmo(int numOfGizmo) {
+		this.numOfGizmo = numOfGizmo;
+	}
+	
+	public void decrementNumOfGizmo(){
+		this.numOfGizmo -= 1;
+	}
+	
+	public boolean checkNumOfGizmo(){
+		if(numOfGizmo <= 4){
 			return true;
 		}
 		else{
