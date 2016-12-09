dada8922dc88a1c0ced74efbbf97c6c4a922e0d5
ALKIN SEN <asen14@ku.edu.tr>
2016-12-09 17:57:09 +0300

    added variables

--- src/game/HadiCezmi.java
+++ src/game/HadiCezmi.java
@@ -6,6 +6,8 @@ package game;
 public class HadiCezmi {
     public static final int FRAME_WIDTH = 800;
     public static final int FRAME_HEIGHT = 800;
+    public static final int BOARD_WIDTH = 500;
+    public static final int BOARD_HEIGHT = 500;
     //GLOBAL VARIABLES SHOULD BE HERE
 
 
