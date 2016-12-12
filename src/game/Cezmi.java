aa0a467ffd7dcfba26f9c067d37a2f05de69e81c
ALKIN SEN <asen14@ku.edu.tr>
2016-12-12 16:45:58 +0300

    fixed cezmi, added keylistener for cezmi

--- src/game/Cezmi.java
+++ src/game/Cezmi.java
@@ -6,120 +6,110 @@ import java.util.Observable;
 /**
  * Created by ASEN14 on 28.11.2016.
  */
-public class Cezmi extends Observable{
+public class Cezmi extends Observable {
 
-	private int radius;
-	private int x;
-	private int y;
-	private Color color;
-	private int vy= 10;
-	private int vx= 10;
-	private int cezmilevel;
+    private int radius;
+    private int x;
+    private int y;
+    private Color color;
+    private int vy = 10;
+    private int vx = 10;
+    private int cezmilevel;
 
-	
-	public Cezmi(int xLoc, int yLoc, int level){
-		
-		x=xLoc;
-		y=yLoc;
-		radius=20;
-		color=Color.RED;
-		cezmilevel=level;
-		
-	}
 
-	public int getRadius() {
-		return radius;
-	}
+    public Cezmi(int xLoc, int yLoc, int level) {
 
-	public void setRadius(int radius) {
-		this.radius = radius;
-	}
+        x = xLoc;
+        y = yLoc;
+        radius = 20;
+        color = Color.RED;
+        cezmilevel = level;
 
-	public int getX() {
-		return x;
-	}
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public int getRadius() {
+        return radius;
+    }
 
-	public int getY() {
-		return y;
-	}
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 
-	public void setY(int y) {
-		this.y = y;
-	}
-	public Color getColor(){
-		return color;
-	}
-	public void setColor(Color newcolor){
-		color=newcolor;
-	}
+    public int getX() {
+        return x;
+    }
 
-	@Override
-	public String toString() {
-		return "Cezmi [radius=" + radius + ", x=" + x + ", y=" + y + "]";
-	}
-	public void moveLeft(){
-		if(x<=240){
-		if(!(x<=10) && !(x>=240)){
-		x = x + vx;
-		}
-		}
-		else if(x>=260){
-			if(!(x<=260) && !(x>=490)){
-				x = x + vx;
-			}
-		}
-		System.out.println("cezmi move left");
-		setChanged();
-		notifyObservers(this);
-		
-	}
-	
-	public void moveRight(){
-		if(x<=240){
-		if(!(x<=10) && !(x>=240)){
-		x = x - vx;
-		}
-		
-		}
-		else if(x>=260){
-			if(!(x<=260) && !(x>=490)){
-			x = x - vx;
-			}
-		}
-		System.out.println("cezmi.moveright");
-		setChanged();
-		notifyObservers(this);
-	}
-	
-	public void moveUp(){
-		if(cezmilevel==2){
-			if(x==0 || x==500){
-				if(!(y<=0) && !(y>=500)){
-					y = y - vy;
-				}
-			}
-		}
-		setChanged();
-		notifyObservers(this);
-	}
-	public void moveDown(){
-		if(cezmilevel==2){
-			if(x==0 || x==500){
-				if(!(y<=0) && !(y>=500)){
-					y = y + vy;
-				}
-			}
-		}
-		setChanged();
-		notifyObservers(this);
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	
-	
-	
-	
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color newcolor) {
+        color = newcolor;
+    }
+
+    @Override
+    public String toString() {
+        return "Cezmi [radius=" + radius + ", x=" + x + ", y=" + y + "]";
+    }
+
+    public void moveLeft() {
+        if (x <= 230 && x >= vx) {
+            x = x - vx;
+        } else if (x >= 270+vx) {
+            x = x - vx;
+        }
+        System.out.println("cezmi move left =" + x);
+        setChanged();
+        notifyObservers(this);
+
+    }
+
+    public void moveRight() {
+        if (x + vx <= 230 ) {
+            x = x + vx;
+        } else if ( x >= 270 && x <= 500 - vx) {
+            x = x + vx;
+        }
+        System.out.println("cezmi move right =" + x);
+        setChanged();
+        notifyObservers(this);
+    }
+
+    public void moveUp() {
+        if (cezmilevel == 2) {
+            if (x == 0 || x == 500) {
+                if (!(y <= 0) && !(y >= 500)) {
+                    y = y - vy;
+                }
+            }
+        }
+        setChanged();
+        notifyObservers(this);
+    }
+
+    public void moveDown() {
+        if (cezmilevel == 2) {
+            if (x == 0 || x == 500) {
+                if (!(y <= 0) && !(y >= 500)) {
+                    y = y + vy;
+                }
+            }
+        }
+        setChanged();
+        notifyObservers(this);
+    }
+
+
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,10 +1,7 @@
 package test;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.File;
+import java.awt.event.*;
+
 import game.*;
 
 import game.HadiCezmi;
@@ -26,13 +23,62 @@ public class test {
 		gameFrame.pack();
 		gameFrame.setVisible(true);
 
+		gameFrame.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keynum = e.getKeyCode();
+				System.out.println(keynum);
+
+				if(keynum == hadi.getCezmi1Left()){
+
+					hadi.moveCezmi(1, "left");
+
+				}else if(keynum == hadi.getCezmi1Right()){
+
+					hadi.moveCezmi(1, "right");
+
+				}else if(keynum == hadi.getCezmi2Left()){
+
+					hadi.moveCezmi(2, "left");
+
+				}else if(keynum == hadi.getCezmi2Right()){
+
+					hadi.moveCezmi(2, "right");
+
+				}else if(keynum == hadi.getTokatLeftKey()){
+
+					hadi.rotateTokat("left");
+
+				}else if(keynum == hadi.getTokatRightKey()){
+
+					hadi.rotateTokat("right");
+
+				}
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+
+			}
+		});
+
+
 		final Timer timer = new Timer(10, new ActionListener() {
 			@Override
 			public void actionPerformed(final ActionEvent e) {
 				gameFrame.repaint();
+				gameFrame.requestFocus();
 			}
 		});
 		timer.start();
+
 
 	}
 
