7d6a399b5df41553baf776bdd74c4b024bd0b374
ALKIN SEN <asen14@ku.edu.tr>
2016-12-14 19:21:42 +0300

    connected start, edit and game frame

--- src/ui/StartFrameController.java
+++ src/ui/StartFrameController.java
@@ -27,63 +27,14 @@ public class StartFrameController {
     public void doAction(String action, HadiCezmi hadiCezmi) {
         switch (action) {
             case "Play":
-                playGame(hadiCezmi);
+                new EditFrame(hadiCezmi);
                 break;
             case "Load":
                 loadGame(hadiCezmi);
                 break;
-            case "Edit":
-                editGame();
-                break;
             default:
                 break;
         }
-    }
-
-    public void playGame(HadiCezmi hadiCezmi) {
-        HadiCezmi hadi = hadiCezmi;
-        GameFrame gameFrame = new GameFrame(hadi);
-        gameFrame.pack();
-        gameFrame.setVisible(true);
-
-        gameFrame.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                int keynum = e.getKeyCode();
-                System.out.println(keynum);
-                if (keynum == hadi.getCezmi1Left()) {
-                    hadi.moveCezmi(1, "left");
-                } else if (keynum == hadi.getCezmi1Right()) {
-                    hadi.moveCezmi(1, "right");
-                } else if (keynum == hadi.getCezmi2Left()) {
-                    hadi.moveCezmi(2, "left");
-                } else if (keynum == hadi.getCezmi2Right()) {
-                    hadi.moveCezmi(2, "right");
-                } else if (keynum == hadi.getTokatLeftKey()) {
-                    hadi.rotateTokat("left");
-                } else if (keynum == hadi.getTokatRightKey()) {
-                    hadi.rotateTokat("right");
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-            }
-        });
-
-        final Timer timer = new Timer(10, new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                System.out.println(Thread.currentThread().getName());
-                gameFrame.repaint();
-                gameFrame.requestFocus();
-            }
-        });
-        timer.start();
     }
 
     public void loadGame(HadiCezmi hadiCezmi) {
