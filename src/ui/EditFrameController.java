b1c7ed613a242dfff856bee450ea9eba57fdc9af
Pinar <Pinar@172.20.114.17>
2016-12-15 17:20:41 +0200

    Edit Mode dan sonra Oyuna başlamak

--- src/ui/EditFrameController.java
+++ src/ui/EditFrameController.java
@@ -1,6 +1,5 @@
 package ui;
 
-<<<<<<< HEAD
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,30 +25,32 @@ public class EditFrameController extends StartFrameController {
             			Color c=g[i][j].getBackground();
             			
             			if(c.equals(Color.yellow)){
-            			hadiCezmi.getBoard().addGizmo("squareTakoz", i*25, j*25);
+            			hadiCezmi.getBoard().addGizmo("SquareTakoz", i*20, j*20);
             			} else if (c.equals(Color.magenta)){
-            				hadiCezmi.getBoard().addGizmo("tokat", i*25, j*25);
+            				hadiCezmi.getBoard().addGizmo("Tokat", i*20, j*20);
             			}else if(c.equals(Color.red)){
-            				hadiCezmi.getBoard().addGizmo("triangularTakoz", i*25, j*25);
+            				hadiCezmi.getBoard().addGizmo("TriangularTakoz", i*20, j*20);
             			}else if(c.equals(Color.orange)){
-            				hadiCezmi.getBoard().addGizmo("firildak", i*25, j*25);
+            				hadiCezmi.getBoard().addGizmo("Firildak", i*20, j*20);
             			}
             		}
             	}
             	for(int i=0;i<12;i++){   		
-            			Color c=g[i][25].getBackground();
+            			Color c=g[i][24].getBackground();
             			
             			if(c.equals(Color.green)){
-            			hadiCezmi.getBoard().changeCezmiPosition(1, i*25);  
+            			hadiCezmi.getBoard().changeCezmiPosition(1, i*20);  
             			}
             	}
             	for(int i=13;i<25;i++){   		
-                		Color c=g[i][25].getBackground();
+                		Color c=g[i][24].getBackground();
                 		if(c.equals(Color.green)){
-                			hadiCezmi.getBoard().changeCezmiPosition(2, i*25);        
+                			hadiCezmi.getBoard().changeCezmiPosition(2, i*20);        
                 		}
                 }
-                hadiCezmi.play();
+            	
+            	play(hadiCezmi);
+            	
                 break;
             case "save":
             	//a
@@ -98,76 +99,6 @@ public class EditFrameController extends StartFrameController {
             @Override
             public void actionPerformed(final ActionEvent e) {
                 System.out.println(Thread.currentThread().getName());
-                gameFrame.repaint();
-                gameFrame.requestFocus();
-            }
-        });
-        timer.start();
-    }
-=======
-import game.HadiCezmi;
->>>>>>> master
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-public class EditFrameController {
-
-    public EditFrameController() {
-
-    }
-
-    public void doAction(String action, HadiCezmi hadiCezmi) {
-        switch (action) {
-            case "Play":
-                playGame(hadiCezmi);
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void playGame(HadiCezmi hadiCezmi) {
-        GameFrame gameFrame = new GameFrame(hadiCezmi);
-        gameFrame.pack();
-        gameFrame.setVisible(true);
-
-        gameFrame.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                int keynum = e.getKeyCode();
-                System.out.println(keynum);
-                if (keynum == hadiCezmi.getCezmi1Left()) {
-                    hadiCezmi.moveCezmi(1, "left");
-                } else if (keynum == hadiCezmi.getCezmi1Right()) {
-                    hadiCezmi.moveCezmi(1, "right");
-                } else if (keynum == hadiCezmi.getCezmi2Left()) {
-                    hadiCezmi.moveCezmi(2, "left");
-                } else if (keynum == hadiCezmi.getCezmi2Right()) {
-                    hadiCezmi.moveCezmi(2, "right");
-                } else if (keynum == hadiCezmi.getTokatLeftKey()) {
-                    hadiCezmi.rotateTokat("left");
-                } else if (keynum == hadiCezmi.getTokatRightKey()) {
-                    hadiCezmi.rotateTokat("right");
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-            }
-        });
-
-        final Timer timer = new Timer(10, new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-//                System.out.println(Thread.currentThread().getName());
                 gameFrame.repaint();
                 gameFrame.requestFocus();
             }
