433fad769199e7c6d1db1f75e96afeb15e64eb37
ALKIN SEN <asen14@ku.edu.tr>
2017-01-05 01:40:23 +0300

    changed cezerye time

--- src/game/Board.java
+++ src/game/Board.java
@@ -266,6 +266,7 @@ public class Board extends Observable{
       //check the situation of cezerye
         if(!cezeryeAppear && countdown == -1){
         	countdown = (int) (1000 + Math.random()*200*25);
+            getCezerye().setCountdown(countdown);
         }else if(!cezeryeAppear && countdown == 0){
         	int xLoc = (int)(Math.random()*24.0) * 20;
         	int yLoc = (int)(Math.random()*18.0) * 20  + 40;
@@ -281,16 +282,20 @@ public class Board extends Observable{
         	cezeryeAppear=true;
             getCezerye().setCezeryeAppear(cezeryeAppear);
         	countdown=1000;
+            getCezerye().setCountdown(countdown);
         }else if(!cezeryeAppear && countdown>0){
         	countdown--;
+            getCezerye().setCountdown(countdown);
         }else if(cezeryeAppear && countdown >0){
         	countdown--;
+            getCezerye().setCountdown(countdown);
         }else if(cezeryeAppear && countdown == 0){
         	cezeryeAppear=false;
             getCezerye().setCezeryeAppear(cezeryeAppear);
             getCezerye().setX(1000);
             getCezerye().setY(1000);
             countdown = (int) (1000 + Math.random()*200*25);
+            getCezerye().setCountdown(countdown);
         }
     }
     public void checkCollisionAndReflectBall(Ball ball, boolean leftPressed, boolean rightPressed) {
@@ -450,6 +455,7 @@ public class Board extends Observable{
                     getCezerye().setX(1000);
                     getCezerye().setY(1000);
                     countdown = (int) (1000 + Math.random()*200*25);
+                    getCezerye().setCountdown(countdown);
 
                     Random rand = new Random();
                     int specialEffectNumber = rand.nextInt(3);
